a46009625d23d62859bfb44279666c99c94d1e88
Revathy1997 <revkutty777@gmail.com>
2017-06-21 13:53:12 +0530

    Create set1(e).py

---
+++ set1(e).py
@@ -0,0 +1,9 @@
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+c=int(input("enter the 3nd number"))
+if a>b and a>c:
+	print(str(a)+" is large")
+elif b>c:
+	print(str(b)+"b is large")
+else:
+	print(str(c)+"c is large")
